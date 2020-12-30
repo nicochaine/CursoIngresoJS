ac1ccb200a8d14aaa6a3aa6c2b94953be0238a0d
Tinki <nicolaschaine@msn.com>
2020-12-30 19:00:46 -0300

    cambios mirando la clase 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,13 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+
+let nombre = prompt("Ingrese su nombre aqui") 
+//let es la VARIABLE que se muestra a traves del NOMBRE = ""
+//promp es para  abrir una ventana emergente a completar
+//alert es para abrir una ventana emergente con un dato
+
+alert(nombre)
+
 }
 
